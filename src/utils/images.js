58f9b3e3b70891099b7f9ecd4458b8b16e696b54
sheffieldAndrew <sheffieldandrew@hotmail.co.uk>
2022-07-26 10:10:54 +0100

    added topic icon to article card

---
+++ src/utils/images.js
@@ -0,0 +1,10 @@
+const images = {
+  football:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVnEUWl1XFDWP6wTayByRPoU8yhoblTgea6FVZJ9Rt2iK1vli3H1UjJEajh5PQ_6yBi5A&usqp=CAU",
+  coding:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3k14K5y2RfdmGfAtdb2UwX7AUIH0QrBHg_YvBm2rnattODpv1orxvdWKIjPLHQUaj37E&usqp=CAU",
+  cooking:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs-mplv2mwdOsY5uHmeQFtwigXj5s6hGKvdjH9xzge6ujtoHwlyjrTZUU_MBLjdQdw0Tk&usqp=CAU",
+};
+
+export default images;
