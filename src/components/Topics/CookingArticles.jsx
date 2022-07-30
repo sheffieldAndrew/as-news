8c21781ee741a6ae85446af1127e9af5de59b421
sheffieldAndrew <sheffieldandrew@hotmail.co.uk>
2022-07-30 13:01:20 +0100

    sorting on category pages complete, styling updated

--- src/components/Topics/CookingArticles.jsx
+++ src/components/Topics/CookingArticles.jsx
@@ -1,25 +1,33 @@
 import { useState, useEffect } from "react";
 import ArticleCard from "../ArticleCard";
 import SortByCategory from "../SortByCategory";
+import axios from "axios";
 
 const CookingArticles = () => {
   const [cookingArticles, setCookingArticles] = useState([]);
-  const [setSort_by] = useState('created_at');
-
+  const [sort_by, setSort_by] = useState("created_at");
+  const [order, setOrder] = useState("desc");
+  const [topic] = useState("coding");
 
   useEffect(() => {
-    fetch(`https://as-nc-news.herokuapp.com/api/articles?topic=cooking`)
-      .then((res) => res.json())
-      .then(({ articles }) => setCookingArticles(articles));
-  }, []);
+    axios
+      .get(`https://as-nc-news.herokuapp.com/api/articles`, {
+        params: {
+          sort_by: sort_by,
+          order: order,
+          topic: topic,
+        },
+      })
+      .then((res) => setCookingArticles(res.data.articles));
+  }, [sort_by, order, topic]);
 
   return (
     <main className="app_articlesList">
       <h2 className="app_articlesList_header">Cooking Articles</h2>
-      <SortByCategory  setSort_by={setSort_by} />
+      <SortByCategory setSort_by={setSort_by} setOrder={setOrder} />
       {cookingArticles.map((article) => {
-          return <ArticleCard article={article} key={article.article_id} />;
-        })}
+        return <ArticleCard article={article} key={article.article_id} />;
+      })}
     </main>
   );
 };
--- src/components/Topics/FootballArticles.jsx
+++ src/components/Topics/FootballArticles.jsx
@@ -1,29 +1,35 @@
 import { useState, useEffect } from "react";
 import ArticleCard from "../ArticleCard";
 import SortByCategory from "../SortByCategory";
+import axios from "axios";
 
 const FootballArticles = () => {
   const [footballArticles, setFootballArticles] = useState([]);
-  const [sort_by, setSort_by] = useState('created_at');
-
+  const [sort_by, setSort_by] = useState("created_at");
+  const [order, setOrder] = useState("desc");
+  const [topic] = useState("football");
 
   useEffect(() => {
-    fetch(`https://as-nc-news.herokuapp.com/api/articles?topic=football`)
-      .then((res) => res.json())
-      .then(({ articles }) => setFootballArticles(articles));
-  }, [sort_by]);
+    axios
+      .get(`https://as-nc-news.herokuapp.com/api/articles`, {
+        params: {
+          sort_by: sort_by,
+          order: order,
+          topic: topic,
+        },
+      })
+      .then((res) => setFootballArticles(res.data.articles));
+  }, [sort_by, order, topic]);
 
   return (
     <main className="app_articlesList">
       <h2 className="app_articlesList_header">Football Articles</h2>
-      <SortByCategory  setSort_by={setSort_by}/>
-      {footballArticles.sort((a,b)=>b.sort_by-a.sort_by).map((article) => {
-  return <ArticleCard article={article} key={article.article_id}/>;
-})}
+      <SortByCategory setSort_by={setSort_by} setOrder={setOrder} />
+      {footballArticles.map((article) => {
+        return <ArticleCard article={article} key={article.article_id} />;
+      })}
     </main>
   );
 };
 
 export default FootballArticles;
-
-
